b001a6ed5bcf21daac5f845934f73df99ff80c69
shritechno <shri.techno89@gmail.com>
2025-07-14 17:00:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 echo "Hello from PHP CI/CD pipeline!";
-echo'</br>;
+echo'</br>';
 echo'</br>';
 echo "It is CI/CD Practice......";
+
+function greet() {
+  $unused = "hello";
+  echo "Hi!";
+}
+
+
 ?>
